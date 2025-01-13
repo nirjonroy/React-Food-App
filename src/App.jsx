8c7070a57378f10347e1acf4e58b18fcca430363
nirjonroy <roynirjon18@gmail.com>
2025-01-13 19:47:38 +0600

    Creating State To Save Food Data and Building The Food List Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import Search from './Components/Search'
+import FoodList from './Components/FoodList';
 
 
 
 function App() {
-  
+  const [foodData, setFoodData] = useState([]);
 
   return (
     <div className="App">
-     <Search />
+     <Search foodData={foodData} setFoodData={setFoodData} />
+
+     <FoodList foodData={foodData}  />
     </div>
   )
 }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "86358afcba5e4d76a5c0a3f67f9dd8db";
-export default function Search(){
+export default function Search({foodData, setFoodData}){
     const [query, setQuery] = useState("Pizza");
     useEffect(()=>{
         async function fetchFood(){
             const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
             const data = await res.json();
             console.log(data.results);
+            setFoodData(data.results);
         }
         fetchFood();
     }, [query])
