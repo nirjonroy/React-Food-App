314013a5193e5e09beccc541f4010e93a67e6f7c
nirjonroy <roynirjon18@gmail.com>
2025-01-13 20:56:26 +0600

    Creating The Search Component

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from "./search.module.css";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "86358afcba5e4d76a5c0a3f67f9dd8db";
 export default function Search({foodData, setFoodData}){
@@ -13,8 +14,8 @@ export default function Search({foodData, setFoodData}){
         fetchFood();
     }, [query])
     return(
-        <div>
-            <input type="text" placeholder="Search"  value={query} onChange={(e) => setQuery(e.target.value)} />
+        <div className={styles.search-container}>
+            <input className={styles.input} type="text" placeholder="Search"  value={query} onChange={(e) => setQuery(e.target.value)} />
         </div>
     )
 }
